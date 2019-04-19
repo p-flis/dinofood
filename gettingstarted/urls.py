906117259d192f85c5c741c30960a047c7b76504
pflis <pawel.flis98@gmail.com>
2019-04-19 22:31:45 +0200

    Pleh

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,7 +15,7 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    #path("", hello.views.index, name="index"),
+    path("", hello.views.index, name="index"),
     #path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
