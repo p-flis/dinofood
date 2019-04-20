de1f657aa7e5cc4348edcc176cc2b85ba83b49cd
JakubBilski <jakub.michal.bilski@gmail.com>
2019-04-20 14:51:56 +0200

    forgot about two things

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
-
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+
 admin.autodiscover()
 
 import hello.views
@@ -15,8 +15,8 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
+    path("", hello.views.index, name="index"),
     #path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
