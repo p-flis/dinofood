062abae604fcd975749549c05ef4007e74ae20e1
JakubBilski <jakub.michal.bilski@gmail.com>
2019-04-20 14:45:34 +0200

    added login option

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,5 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-     return HttpResponse('Hello Karol!')
-    #return render(request, "index.html")
-
-
-
+     #return HttpResponse('Hello Karol!')
+     return render(request, "home.html")
