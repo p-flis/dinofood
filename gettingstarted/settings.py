8e0948b510cb3af3733e98c1cc832ee61b4b700d
karolk98 <47919588+karolk98@users.noreply.github.com>
2019-04-20 18:13:31 +0200

    settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dinofood',
+        'USER': 'postgres',
+        'PASSWORD': 'cebula123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=myschema'
+        }
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
