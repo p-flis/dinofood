eda84c8042860c21950d4a0ce8c08721d75539b4
JakubBilski <jakub.michal.bilski@gmail.com>
2019-05-08 20:35:41 +0200

    set postgress locally, created migrations
    deleted everything, downloaded everything, deleted migrations, created them from the beginning

--- core/settings.py
+++ core/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dinofood',
+        'NAME': 'galaktykakurwix',
         'USER': 'postgres',
-        'PASSWORD': 'cebula123',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
         'OPTIONS': {
