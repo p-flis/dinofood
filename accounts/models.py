fa24fc21693ac5554537f183643434d93d8ec667
JakubBilski <jakub.michal.bilski@gmail.com>
2019-05-08 20:49:32 +0200

    changed user to inherit from abstractuser
    some of the class properties should be probably changed, and some will be inherited, but I will change it next

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
+class User(AbstractUser):
     nick = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     admin = models.BooleanField(default=False)
+    def __str__(self):
+        return self.email
