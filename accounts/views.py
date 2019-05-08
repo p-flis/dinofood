c278c72b3fe0cbd41da0e3afa2af674792f0bfe1
JakubBilski <jakub.michal.bilski@gmail.com>
2019-05-08 23:06:05 +0200

    changed signup to use custom user form

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
