fe4b5d420ee4853f683d8f4c616df6e42aad7414
JakubBilski <jakub.michal.bilski@gmail.com>
2019-05-08 22:49:12 +0200

    deleted email and password properties of user
    they should be inherited from AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     nick = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
     admin = models.BooleanField(default=False)
     def __str__(self):
         return self.email
