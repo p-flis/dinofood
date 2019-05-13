010389afa3178c9c504bab385509cf830243100f
JakubBrojacz <38508437+JakubBrojacz@users.noreply.github.com>
2019-05-13 15:31:25 +0200

    Better search

--- base/dish_views/search_views.py
+++ base/dish_views/search_views.py
@@ -22,20 +22,28 @@ def recipe_search(request):
 
         ingredients_in_recipe_len = len(ingredients_in_recipe)
 
-        # recipes_affordable = recipes_with_required_ings.exclude(ingredients__name__in=ingredients_in_fridge)
-        recipes_not_affordable = Dish.objects\
-            .exclude(ingredients__name__in=ingredients_in_fridge)\
-            .annotate(recipe_price=Sum(F('dishdetails__quantity')*F('dishdetails__ingredient__price')))\
+        search_result = Dish.objects
+
+        if ingredients_in_fridge and any(ingredients_in_fridge):
+            search_result = search_result \
+                .exclude(ingredients__name__in=ingredients_in_fridge)\
+
+        search_result = search_result\
+            .annotate(recipe_price=Sum(F('dishdetails__quantity') * F('dishdetails__ingredient__price'))) \
             .filter(recipe_price__gt=extra_money)
-        ids_not_affordable = [item.id for item in recipes_not_affordable.all()]
-        recipes_affordable = Dish.objects.exclude(id__in=ids_not_affordable)
 
-        recipes_with_required_ings = recipes_affordable \
-            .filter(ingredients__name__in=ingredients_in_recipe) \
-            .annotate(ing_num=Count('ingredients')) \
-            .filter(ing_num=ingredients_in_recipe_len)
+        print(search_result.query)
+        ids_not_affordable = [item.id for item in search_result.all()]
+        search_result = Dish.objects.exclude(id__in=ids_not_affordable)
 
-        return render(request, "food/recipe.html", {"list_items": recipes_with_required_ings.all()})
+        if ingredients_in_recipe and any(ingredients_in_recipe):
+            search_result = search_result \
+                .filter(ingredients__name__in=ingredients_in_recipe) \
+                .annotate(ing_num=Count('ingredients')) \
+                .filter(ing_num=ingredients_in_recipe_len)
+
+        print(search_result.query)
+        return render(request, "food/recipe.html", {"list_items": search_result.all()})
 
 
 
