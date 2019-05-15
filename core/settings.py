259fb848d1d6cfc9aa723a700f26be47cc51e678
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-05-15 08:26:03 +0200

    small change
    edited credentials

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'galaktykakurwix',
+#         'NAME': 'galaktyka',
 #         'USER': 'postgres',
 #         'PASSWORD': 'admin',
 #         'HOST': 'localhost',
