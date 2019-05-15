0c8f0432d1cbd8a61b7daa7bdbcecfb62a6bf9a5
karolk98 <47919588+karolk98@users.noreply.github.com>
2019-05-15 18:10:21 +0200

    Recipes update

--- base/models.py
+++ base/models.py
@@ -1,5 +1,9 @@
+
 from django.db import models
 from accounts import models as accounts_models
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+
 
 # Create your models here.
 class Greeting(models.Model):
@@ -7,11 +11,11 @@ class Greeting(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -37,5 +41,11 @@ class DishDetails(models.Model):
 class Rating(models.Model):
     user = models.ForeignKey(accounts_models.User, on_delete=models.CASCADE)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    rating = models.IntegerField(blank=True,null=True)
+    rating = models.IntegerField(blank=True, null=True)
     favourite = models.BooleanField(default=False)
+
+
+@receiver(pre_delete, sender=Ingredient)
+def delete_related_recipes(sender, instance, using, **kwargs):
+    for recipe in Ingredient.objects.get(id=instance.id).dish_set.all():
+        recipe.delete()
