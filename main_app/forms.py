11bcc92a37070cbf7f1bca008d8ba4050cffb1ad
JakubBilski <jakub.michal.bilski@gmail.com>
2019-05-22 17:05:01 +0200

    changed ingredient form to modelform, moved form.as_p to child forms templates

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from .models import *
 
-
-class IngredientForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(IngredientForm, self).__init__(*args, **kwargs)
-        self.fields['name'] = forms.CharField()
-        self.fields['price'] = forms.DecimalField(min_value=0, max_digits=8, decimal_places=2)
-
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = [
+            "name",
+            'price',
+        ]
 
 class RecipeForm(forms.ModelForm):
     class Meta:
