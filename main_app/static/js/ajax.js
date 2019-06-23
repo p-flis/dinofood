bdf0f88a1cb1c8446f35dd5cf1c12f63f6b89b3f
pflis <pawel.flis98@gmail.com>
2019-06-23 23:48:29 +0200

    AJAX adding ingredients to new recipe

--- main_app/static/js/ajax.js
+++ main_app/static/js/ajax.js
@@ -1,14 +1,18 @@
 
 function searchSuccess(data, textStatus, jqXHR, thisNode)
 {
-    $('#search-results').html(data);
+    var tmp = data.split('\n');
+    //window.alert(data);
+    $(tmp[0]).html(data);
 }
 
 function happyFunction(e, thisNode) {
         $.ajax({
             type: "POST",
-            url: "/recipe/work/",
+            url: "ajax/recipe/ing_unit",
             data: {
+                search_text: thisNode.value,
+                form_id: thisNode.id,
                'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
             },
             success: searchSuccess,
